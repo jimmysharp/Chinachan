6b2b32f51b8f893a64fc5b776e8ea88e7c3b7472
sharp <jimmy.sharptech@gmail.com>
2018-07-30 00:52:04 +0900

    [Mod] Timer利用可能なよう修正

---
+++ app/src/main/java/io/github/jimmysharp/chinachan/MainApplication.kt
@@ -0,0 +1,12 @@
+package io.github.jimmysharp.chinachan
+
+import android.app.Application
+import timber.log.Timber
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
+}
